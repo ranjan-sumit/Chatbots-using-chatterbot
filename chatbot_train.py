81482d2a471a6e7f42a8ec7f8754becdd108cfd0
Sumit Ranjan <36401733+ranjan-sumit@users.noreply.github.com>
2018-03-18 02:14:10 +0530

    Chatbot
    Chatbot

---
+++ chatbot_train.py
@@ -0,0 +1,18 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+for files in os.listdir('C:/Sumit/Data Science/Chatbot using chatterbot/chatterbot-corpus-master/chatterbot_corpus/data/english'):
+             data = open('C:/Sumit/Data Science/Chatbot using chatterbot/chatterbot-corpus-master/chatterbot_corpus/data/english/' + files ,'r').readlines()
+             bot.train(data)
+while True:
+         message = input('You:')
+         if message.strip() != 'Bye': 
+                reply = bot.get_response(message)
+                print('ChatBot:',reply)
+         if message.strip() == 'Bye': 
+                print('ChatBot : Bye')
+                break
